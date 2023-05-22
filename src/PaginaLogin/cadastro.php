007b7da270f49cb9f5f18e738331a618bcbf5e97
Joao Vitor Siqueira <jvitorsiqueira99@gmail.com>
2023-05-22 17:54:09 -0300

    animacao das labels

--- src/PaginaLogin/cadastro.php
+++ src/PaginaLogin/cadastro.php
@@ -17,11 +17,11 @@
         <h1>Cadastro</h1>
         <form action="#">
             <div class="control">
-                <label for="name">Nome</label>
+                <label for="name" id="lbName">Nome</label>
                 <input type="text" name="name" id="name">
             </div>
             <div class="control">
-                <label for="password">Senha</label>
+                <label for="password" id="lbPassword">Senha</label>
                 <input type="password" name="password" id="password">
             </div>
             <div class="control">
@@ -34,6 +34,27 @@
         
 
         
-        <script src="" async defer></script>
+        <script defer>
+            const username = document.getElementById("name")
+            const nameLbl = document.getElementById("lbName")
+            const password = document.getElementById("password")
+            const passwordLbl = document.getElementById("lbPassword")
+
+            username.addEventListener("focus",() => {
+                nameLbl.style.color= "white"
+            })
+            username.addEventListener("blur",() => {
+                nameLbl.style.color = "lightgray"
+            })
+
+            password.addEventListener("focus",() => {
+                passwordLbl.style.color= "white"
+            })
+            password.addEventListener("blur",() => {
+                passwordLbl.style.color = "lightgray"
+            })
+
+
+        </script>
     </body>
 </html>
