09ea8914d094533ce191980c063dc0725644df19
Joao Vitor Siqueira <jvitorsiqueira99@gmail.com>
2023-05-22 16:17:09 -0300

    Pagina de login

---
+++ src/PaginaLogin/cadastro.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="login.css">
+        
+    </head>
+    <body>
+       
+       
+    <div class="loginContainer">
+        <h1>Cadastro</h1>
+        <form action="#">
+            <div class="control">
+                <label for="name">Nome</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="control">
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div class="control">
+                <input type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
+
+    
+        
+
+        
+        <script src="" async defer></script>
+    </body>
+</html>
---
+++ src/PaginaLogin/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="login.css">
+        
+    </head>
+    <body>
+       
+       
+    <div class="loginContainer">
+        <h1>Login</h1>
+        <form action="#">
+            <div class="control">
+                <label for="name">Nome</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="control">
+                <label for="password">Senha</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div class="control">
+                <input type="submit" value="Entrar">
+            </div>
+            <div class="link">
+                <a href="cadastro.php">Não possui uma conta?</a>
+            </div>
+
+        </form>
+    </div>
+
+    
+        
+
+        
+        <script src="" async defer></script>
+    </body>
+</html>
