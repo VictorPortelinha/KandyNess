f0679bb4ad0d1214dfc512453a5f9a5cf183151e
Joao Vitor Siqueira <siqueira.vitor@pucpr.edu.br>
2023-05-24 10:41:25 -0300

    table pgCompra

--- src/PaginaCompra/compra.php
+++ src/PaginaCompra/compra.php
@@ -1,5 +1,5 @@
 <?php
-require "../dbConnection/queries.php";
+// require "../dbConnection/queries.php";
 
 ?>
 
@@ -12,23 +12,24 @@ require "../dbConnection/queries.php";
     <title>Página de compra</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="compraStyles/carrinho.css">
 </head>
 <body>
 
     <?php include "../HeaderKandyness/HeaderKandyness.php";
-        $idProduto = $_GET['idProduto'];
-        $idLoja = $_GET['idLoja'];
-        $rows = selectProdutoById($idProduto,$idLoja);
-        foreach($rows as $value=>$result):
-            $idProduto = $result['id'];
-            $nomeProduto = $result['nome']; //valores vindos da query do banco de dados
-            $descricaoProduto = $result['descricao'];
-            $precoProduto = $result['valor'];
-            $categoriaProduto = $result['categoria'];
-        endforeach;
+        // $idProduto = $_GET['idProduto'];
+        // $idLoja = $_GET['idLoja'];
+        // $rows = selectProdutoById($idProduto,$idLoja);
+        // foreach($rows as $value=>$result):
+        //     $idProduto = $result['id'];
+        //     $nomeProduto = $result['nome']; //valores vindos da query do banco de dados
+        //     $descricaoProduto = $result['descricao'];
+        //     $precoProduto = $result['valor'];
+        //     $categoriaProduto = $result['categoria'];
+        // endforeach;
     ?>
 
-    <div id="openAdd" class="absoluteLeft">
+    <!-- <div id="openAdd" class="absoluteLeft">
         <button class="add">
             <div class="vert"></div>
             <div class="hor"></div>
@@ -43,122 +44,51 @@ require "../dbConnection/queries.php";
             <img src=" ?>" alt="">
         </div>
         <div class="cardContent">
-            <div class="cardTitle"><?php echo $nomeProduto ?></div>
-            <div style="margin-top: 4px;" class="categoriaLoja"><?php echo $categoriaProduto ?></div>
+            <div class="cardTitle"><?php //echo $nomeProduto ?></div>
+            <div style="margin-top: 4px;" class="categoriaLoja"><?php //echo $categoriaProduto ?></div>
             <div class="divider"></div>
-            <div class="cardDesc"><?php echo "Descrição do produto:" . $descricaoProduto ?></div>
+            <div class="cardDesc"><?php //echo "Descrição do produto:" . $descricaoProduto ?></div>
             <div class="divider"></div>
-            <div class="price"><?php echo "Preço: R$ " . $precoProduto ?></div>
+            <div class="price"><?php //echo "Preço: R$ " . $precoProduto ?></div>
         </div>
-    </div>
+    </div> -->
 
-    
+    <div class="containerCarrinho">
+        <table>
+            <thead>
+                <tr>
+                    <th>Produto</th>
+                    <th>Quantidade</th>
+                    <th>Remover</th>
+                    <th>Preço</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Laranja</td>
+                    <td class="flex">
+                        <div class="decrement"></div>
+                        <div class="num">2</div>
+                        <div class="increment"></div>
+                    </td>
+                    <td>
+                        <div class="remove"></div>
+                    </td>
+                    <td>R$ 50.00</td>
+                    
+ 
+                </tr>
+                <tr>
+                    <td>Laranja</td>
+                    <td> 2 </td>
+                    <td><button>remover</button></td>
+                    <td>R$ 50.00</td>
+ 
+                </tr>
+            </tbody>
+            
+        </table>
+    </div>
     
 </body>
 
-
-<style>
-    
-.card{
-    background-color: #f5f5f5;
-    width: 250px;
-    height: 320px;
-    display: flex;
-    flex-direction: column;
-    font-family: "Roboto",sans-serif;
-    font-weight: lighter;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.4);
-}
-
-.imgContainer{
-    width: 100%;
-    height: 150px;
-    position: relative;
-}
-.imgContainer img{
-    width: 100%;
-    height: 100%;
-}
-
-.cardContent{
-   display: flex;
-   flex-direction: column;
-   padding-left: 10px;
-   padding-right: 10px;
-}
-
-.material-symbols-outlined {
-  font-variation-settings:
-  'FILL' 0,
-  'wght' 300,
-  'GRAD' 0,
-  'opsz' 20
-}
-
-.iconContainer{
-   height: 30px;
-   width: 30px;
-   border-radius: 5vmin;
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.4);
-}
-
-.iconContainer:hover{
-    cursor: pointer;
-    filter: brightness(0.8);
-}
-
-.red{
-    background-color: #F44336;
-}
-
-.blue{
-    background-color: #2196F3;
-}
-
-.join{
-    margin-left: -10px;
-}
-
-.crud{
-    display: flex;
-    gap: 3px;
-    position: absolute;
-    right: 0px;
-    top: 0;
-}
-
-.categoriaLoja{
-    margin-top: 2px;
-    color: hsl(0, 0%, 30%);
-    font-size: 12px;
-}
-
-.cardTitle{
-    margin-top: 12px;
-    font-weight: bold;
-}
-
-.divider{
-    background-color: hsl(0, 0%, 80%);
-    height: 0.08px;
-    margin-top: 10px;
-    
-}
-.cardDesc{
-    font-size: 11px;
-    color: hsl(0, 0%, 40%);
-    margin-top: 12px;
-    word-wrap: break-word;
-    height: 36px;
-}
-
-.price{
-    color: blueviolet;
-    font-weight: 600;
-    position: relative;
-    top: 9px;
-}
-</style>
